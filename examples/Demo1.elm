986c6208982e89c465aff611b86ce8a9148644c7
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-04-06 10:29:30 -0700

    elm-format-0.2.0-alpha on the examples

--- examples/Demo1.elm
+++ examples/Demo1.elm
@@ -5,16 +5,16 @@ import Window
 
 
 view =
-    Layout.top
-        48
-        (Layout.placeholder "header")
-        ([1..25]
-            |> List.map (\n -> "Item " ++ (toString n))
-            |> List.map Layout.placeholder
-            |> Layout.list 32
-            |> Layout.inset 8
-        )
+  Layout.top
+    48
+    (Layout.placeholder "header")
+    ([1..25]
+      |> List.map (\n -> "Item " ++ (toString n))
+      |> List.map Layout.placeholder
+      |> Layout.list 32
+      |> Layout.inset 8
+    )
 
 
 main =
-    Layout.toFullWindow (Signal.constant view)
+  Layout.toFullWindow (Signal.constant view)
